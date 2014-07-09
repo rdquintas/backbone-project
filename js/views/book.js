05b4147c6257c58a116717c1606e19c22b020ca0
Ricardo Quintas <rqcenter-github@yahoo.com>
2014-07-09 21:47:07 +0100

    initial commit

---
+++ js/views/book.js
@@ -0,0 +1,26 @@
+var app = app || {};
+
+app.BookView = Backbone.View.extend({
+    tagName: 'div',
+    className: 'bookContainer',
+    template: _.template($('#bookTemplate').html()),
+
+    events: {
+        'click .delete': 'deleteBook'
+    },
+ 
+    deleteBook: function() {
+        // Delete model
+        this.model.destroy();
+        // Delete view
+        this.remove();
+    },
+
+    render: function() {
+        // tmpl is a function that takes a JSON object and returns html
+        // this.el is what we defined in tagName. use $el to get access
+        // to jQuery html() function
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+    }
+});
